c6a7a6baf912ef9aa3f08e5dc1ccbcd903328c2f
Mircea N <nunu.mircea@gmail.com>
2023-04-22 09:42:52 -0400

    math

---
+++ geometry.py
@@ -0,0 +1,7 @@
+
+import math
+def area_of_rectangle(rectangle_lenght, rectangle_breadth):
+    return rectangle_lenght*rectangle_breadth
+
+def area_of_circle(radius):
+    return math.pi*radius**2
