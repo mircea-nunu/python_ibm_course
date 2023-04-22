ac15c57153e67452815002d53a8223be1695005d
Mircea N <nunu.mircea@gmail.com>
2023-04-22 10:03:31 -0400

    samples pylint
    Signed-off-by: Mircea N <nunu.mircea@gmail.com>

---
+++ sample2.py
@@ -0,0 +1,16 @@
+"""
+This module has math operation functions
+"""
+def add(number1, number2):
+    """
+    This module returns the sum of 2 given numbers
+    """
+    return number1 + number2
+
+NUM1 = 4
+NUM2 = 5
+TOTAL = add(NUM1, NUM2)
+
+#results=()
+
+print(f"The sum of {NUM1} and {NUM2} is {TOTAL}")
